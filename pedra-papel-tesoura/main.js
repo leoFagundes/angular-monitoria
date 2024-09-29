6d83fc4699062ca758d015aa9addd53b0ffab688
leoFagundes <leofagundes2015@gmail.com>
2024-09-29 00:16:38 -0300

    update game

--- pedra-papel-tesoura/main.js
+++ pedra-papel-tesoura/main.js
@@ -8,29 +8,63 @@ var statusElement = document.getElementById("status");
 var pedraButton = document.getElementById("pedra");
 var papelButton = document.getElementById("papel");
 var tesouraButton = document.getElementById("tesoura");
+var maoRight = document.getElementById("mao-right");
+var maoLeft = document.getElementById("mao-left");
+var monitor1 = document.getElementById("monitor1");
+var monitor2 = document.getElementById("monitor2");
 // Algoritmo
 pedraButton.addEventListener("click", function () {
-    jogar(1);
+  shake(1);
 });
 papelButton.addEventListener("click", function () {
-    jogar(2);
+  shake(2);
 });
 tesouraButton.addEventListener("click", function () {
-    jogar(3);
+  shake(3);
 });
+
+function shake(escolha) {
+  maoRight.classList.add("shake");
+  maoLeft.classList.add("shake");
+  setTimeout(() => {
+    jogar(escolha);
+    maoRight.classList.remove("shake");
+    maoLeft.classList.remove("shake");
+  }, 1000);
+}
+
 function jogar(escolha) {
-    var compMove = Math.floor(Math.random() * 3 + 1); // 1, 2, 3
-    if (jogadas[escolha + 1] === jogadas[compMove]) {
-        derrota++;
-    }
-    else if (jogadas[escolha - 1] === jogadas[compMove]) {
-        vitoria++;
-    }
-    else {
-        emapte++;
-    }
-    var resultado = "Seu Movimento = ".concat(jogadas[escolha], " | ").concat(jogadas[compMove], " = Movimento Computador");
-    resulJogo.textContent = resultado;
-    var status = "Vit\u00F3rias = ".concat(vitoria, " Derrotas = ").concat(derrota, " Empates = ").concat(emapte);
-    statusElement.textContent = status;
+  var compMove = Math.floor(Math.random() * 3 + 1); // 1, 2, 3
+
+  monitor2.classList.remove("monitor2-fala");
+  monitor1.classList.remove("monitor1-fala");
+
+  maoRight.classList.remove("mao-pedra-right");
+  maoRight.classList.remove("mao-papel-right");
+  maoRight.classList.remove("mao-tesoura-right");
+
+  maoLeft.classList.remove("mao-pedra-left");
+  maoLeft.classList.remove("mao-papel-left");
+  maoLeft.classList.remove("mao-tesoura-left");
+
+  maoRight.classList.add(`mao-${jogadas[escolha].toLowerCase()}-right`);
+  maoLeft.classList.add(`mao-${jogadas[compMove].toLowerCase()}-left`);
+
+  if (jogadas[escolha + 1] === jogadas[compMove]) {
+    derrota++;
+    monitor2.classList.add("monitor2-fala");
+  } else if (jogadas[escolha - 1] === jogadas[compMove]) {
+    vitoria++;
+    monitor1.classList.add("monitor1-fala");
+  } else {
+    emapte++;
+  }
+  var resultado = `${jogadas[escolha]} X ${jogadas[compMove]}`;
+
+  resulJogo.textContent = resultado;
+  var status = "Vit\u00F3rias = "
+    .concat(vitoria, " Derrotas = ")
+    .concat(derrota, " Empates = ")
+    .concat(emapte);
+  statusElement.textContent = status;
 }
--- pedra-papel-tesoura/main.ts
+++ pedra-papel-tesoura/main.ts
@@ -12,33 +12,61 @@ const statusElement = document.getElementById("status") as HTMLElement;
 const pedraButton = document.getElementById("pedra") as HTMLElement;
 const papelButton = document.getElementById("papel") as HTMLElement;
 const tesouraButton = document.getElementById("tesoura") as HTMLElement;
+const maoRight = document.getElementById("mao-right") as HTMLElement;
+const maoLeft = document.getElementById("mao-left") as HTMLElement;
+const monitor1 = document.getElementById("monitor1") as HTMLElement;
+const monitor2 = document.getElementById("monitor2") as HTMLElement;
 
 // Algoritmo
 
-pedraButton.addEventListener("click", () => {
-  jogar(1);
+pedraButton.addEventListener("click", function () {
+  shake(1);
+});
+papelButton.addEventListener("click", function () {
+  shake(2);
+});
+tesouraButton.addEventListener("click", function () {
+  shake(3);
 });
 
-papelButton.addEventListener("click", () => {
-  jogar(2);
-});
-
-tesouraButton.addEventListener("click", () => {
-  jogar(3);
-});
+function shake(escolha) {
+  maoRight.classList.add("shake");
+  maoLeft.classList.add("shake");
+  setTimeout(() => {
+    jogar(escolha);
+    maoRight.classList.remove("shake");
+    maoLeft.classList.remove("shake");
+  }, 1000);
+}
 
 function jogar(escolha) {
   const compMove = Math.floor(Math.random() * 3 + 1); // 1, 2, 3
 
+  monitor2.classList.remove("monitor2-fala");
+  monitor1.classList.remove("monitor1-fala");
+
+  maoRight.classList.remove("mao-pedra-right");
+  maoRight.classList.remove("mao-papel-right");
+  maoRight.classList.remove("mao-tesoura-right");
+
+  maoLeft.classList.remove("mao-pedra-left");
+  maoLeft.classList.remove("mao-papel-left");
+  maoLeft.classList.remove("mao-tesoura-left");
+
+  maoRight.classList.add(`mao-${jogadas[escolha].toLowerCase()}-right`);
+  maoLeft.classList.add(`mao-${jogadas[compMove].toLowerCase()}-left`);
+
   if (jogadas[escolha + 1] === jogadas[compMove]) {
     derrota++;
+    monitor2.classList.add("monitor2-fala");
   } else if (jogadas[escolha - 1] === jogadas[compMove]) {
     vitoria++;
+    monitor1.classList.add("monitor1-fala");
   } else {
     emapte++;
   }
 
-  const resultado = `Seu Movimento = ${jogadas[escolha]} | ${jogadas[compMove]} = Movimento Computador`;
+  const resultado = `${jogadas[escolha]} X ${jogadas[compMove]}`;
 
   resulJogo.textContent = resultado;
 
